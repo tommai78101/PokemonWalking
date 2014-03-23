f774de4ba72f901c96eed7514c31b3a07eccd111
tommai78101 <tom.mai78101@gmail.com>
2014-03-23 21:24:43 +0800

    Added a new test area to play with.

--- src/level/WorldConstants.java
+++ src/level/WorldConstants.java
@@ -7,14 +7,15 @@ import resources.Art;
 
 public class WorldConstants {
 	private WorldConstants() {
-		
+		//Add/delete a map area, update everything shown below.
 	}
 	
 	public static final int TEST_WORLD_1 = 0x01;
 	public static final int TEST_WORLD_2 = 0x02;
+	public static final int TEST_WORLD_3 = 0x03;
 	
 	//Unimportant at the moment.
-	//private static final int MAX_AREAS = 2;
+	//private static final int MAX_AREAS = 3;
 
 	public static Area convertToArea(int areaID) {
 		switch (areaID) {
@@ -22,6 +23,8 @@ public class WorldConstants {
 				return new Area(Art.testArea, TEST_WORLD_1);
 			case TEST_WORLD_2:
 				return new Area(Art.testArea2, TEST_WORLD_2);
+			case TEST_WORLD_3:
+				return new Area(Art.testArea3, TEST_WORLD_3);
 			default:
 				return null;
 		}
@@ -31,6 +34,7 @@ public class WorldConstants {
 		List<Area> result = new ArrayList<Area>();
 		result.add(new Area(Art.testArea, TEST_WORLD_1));
 		result.add(new Area(Art.testArea2, TEST_WORLD_2));
+		result.add(new Area(Art.testArea3, TEST_WORLD_3));
 		return result;
 	}
 }
