375b6c67a799a27a21b674f49d821eaebadecc13
tommai78101 <tom.mai78101@gmail.com>
2014-03-23 21:26:23 +0800

    Changed the values to static globals. They are truly useful, as I have found.

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -10,10 +10,10 @@ import abstracts.Tile;
 //import abstracts.BaseWorld;
 
 public class Player extends Entity {
-	private final int UP = 2;
-	private final int DOWN = 0;
-	private final int LEFT = 1;
-	private final int RIGHT = 3;
+	public static final int UP = 2;
+	public static final int DOWN = 0;
+	public static final int LEFT = 1;
+	public static final int RIGHT = 3;
 	
 	public Keys keys;
 	
