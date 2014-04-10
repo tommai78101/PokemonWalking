3ffc9d3ea02aa41d290cdb5f4b6234fc060be063
tom_mai78101 <tom.mai78101@gmail.com>
2014-04-10 10:00:08 +0800

    Refactoring the codes.

--- src/screen/Dialogue.java
+++ src/screen/Dialogue.java
@@ -41,6 +41,9 @@ public class Dialogue {
 	private boolean nextTick;
 	private byte arrowTickSpeed;
 	private boolean showDialog;
+	private int firstLineIterator;
+	private int secondLineIterator;
+	private int dialogKeyID;
 	
 	private Keys input;
 	
@@ -53,45 +56,6 @@ public class Dialogue {
 		this.tokenPointer = 0;
 		this.dialogs = new HashMap<Integer, Boolean>();
 	}
-	
-	//	public void tick_old() {
-	//		if (this.showDialog) {
-	//			if (!NewInputHandler.inputsAreLocked())
-	//				NewInputHandler.lockInputs();
-	//			this.tickSpeed--;
-	//			if (this.tickSpeed < 0) {
-	//				if (this.totalStringPointer < this.dialogueText.length() && !this.next) {
-	//					this.stringPointer++;
-	//					this.totalStringPointer++;
-	//				}
-	//				this.tickSpeed = 0x2;
-	//			}
-	//			this.arrowTickSpeed--;
-	//			if (this.arrowTickSpeed < 0) {
-	//				this.nextTick = !this.nextTick;
-	//				this.arrowTickSpeed = 0x6;
-	//			}
-	//			if (this.next) {
-	//				if (input.Z.isPressedDown || input.Z.isTappedDown
-	//						|| input.X.isTappedDown || input.X.isPressedDown
-	//						|| input.SLASH.isTappedDown || input.SLASH.isPressedDown
-	//						|| input.PERIOD.isTappedDown || input.PERIOD.isPressedDown) {
-	//					if (this.totalStringPointer < this.dialogueText.length()) {
-	//						this.firstLineFull = false;
-	//						this.secondLineFull = false;
-	//						this.firstLinePointer = 0;
-	//						this.beginningPointer += this.secondLinePointer;
-	//						this.secondLinePointer = 0;
-	//						this.stringPointer = 0;
-	//						this.next = false;
-	//					}
-	//					else {
-	//						this.hideDialog();
-	//					}
-	//				}
-	//			}
-	//		}
-	//	}
 	
 	public void renderDialog(BaseScreen output, int x, int y, int centerWidth, int centerHeight) {
 		if (this.showDialog) {
@@ -122,34 +86,6 @@ public class Dialogue {
 		}
 	}
 	
-	//	/**
-	//	 * Displays a dialog box showing the text message that was passed in as a parameter. This method
-	//	 * can lock player's inputs while the dialog is displayed onto the screen.
-	//	 * 
-	//	 * @param text
-	//	 *            Pass a String string to make it display the message to the player.
-	//	 * @param dialogID
-	//	 *            This is currently unimplemented.
-	//	 * @return Nothing.
-	//	 * */
-	//	public void displayDialog(String text, int dialogID) {
-	//		if (this.dialogs.containsKey(dialogID)) {
-	//			//TODO: DO SOMETHING WHEN dialogs HAVE BEEN REACHED ALREADY.
-	//			if (this.dialogs.get(dialogID).booleanValue()) { return; }
-	//		}
-	//		if (text != null && !text.isEmpty() && !this.showDialog) {
-	//			this.showDialog = true;
-	//			this.dialogueText = text;
-	//			this.tokens = this.dialogueText.split("\\s");
-	//			this.tokenPointer = 0;
-	//			this.beginningPointer = 0;
-	//			this.firstLinePointer = 0;
-	//			this.secondLinePointer = 0;
-	//			this.totalStringPointer = 0;
-	//			this.dialogs.put(dialogID, false);
-	//		}
-	//	}
-	
 	/**
 	 * Hides the dialog by making it disappear from the screen.
 	 * 
@@ -164,116 +100,6 @@ public class Dialogue {
 		this.setDialogCheckpoint();
 		NewInputHandler.unlockInputs();
 	}
-	
-	//	/**
-	//	 * Tells the Graphics object to draw text onto the screen.
-	//	 * 
-	//	 * <p>
-	//	 * There are many complicated situations where texts can overflow around the dialog boxes, causing it to show up glitchy. Using lots of
-	//	 * conditional checkings, I was able to confine the glitchiness down to a minimum.
-	//	 * 
-	//	 * <p>
-	//	 * In order to use this, this method must be placed somewhere where there is an easy way to pass the Graphics object to this. The Graphics object
-	//	 * is obtained by using getDrawGraphics() from the BufferStrategy, which is created from the Canvas AWT component.
-	//	 * 
-	//	 * @param graphics
-	//	 *            The Graphics object used for drawing texts using custom
-	//	 *            fonts.
-	//	 * @return Nothing.
-	//	 * 
-	//	 * */
-	//	public void renderTextGraphics(Graphics g) {
-	//		if (this.dialogueText != null && !this.dialogueText.isEmpty()) {
-	//			//The game uses 8f FONT when shown on the screen. It is scaled by GAME_SCALE.
-	//			//Text are drawn with positive X = RIGHT, positive Y = UP. Not the other way around.
-	//			g.setColor(Color.black);
-	//			g.setFont(Art.font.deriveFont(Font.PLAIN, 24f));
-	//			if (this.totalStringPointer <= this.dialogueText.length()) {
-	//				//				//Handles one word sentences only.
-	//				//				if (this.stringPointer > this.dialogueText.length()) {
-	//				//					this.stringPointer = this.dialogueText.length();
-	//				//					this.next = true;
-	//				//					g.drawString(this.dialogueText.substring(this.beginningPointer, this.beginningPointer + this.stringPointer), Dialogue.getDialogueTextStartingX(), Dialogue.getDialogueTextStartingY());
-	//				//					return;
-	//				//				}
-	//				
-	//				if (this.totalStringPointer == this.dialogueText.length()) {
-	//					if ((this.totalStringPointer - this.beginningPointer) <= MAX_STRING_LENGTH) {
-	//						g.drawString(this.dialogueText.substring(this.beginningPointer, this.totalStringPointer), Dialogue.getDialogueTextStartingX(), Dialogue.getDialogueTextStartingY());
-	//					}
-	//					else {
-	//						g.drawString(this.dialogueText.substring(this.beginningPointer, this.beginningPointer + this.firstLinePointer), Dialogue.getDialogueTextStartingX(), Dialogue.getDialogueTextStartingY());
-	//						g.drawString(this.dialogueText.substring(this.beginningPointer + this.firstLinePointer, this.totalStringPointer), Dialogue.getDialogueTextStartingX(), Dialogue.getDialogueTextSecondLineStartingY());
-	//					}
-	//					this.next = true;
-	//					return;
-	//				}
-	//				
-	//				//Handles more than one word.
-	//				String text = this.tokens[this.tokenPointer];
-	//				if (this.firstLinePointer + text.length() <= MAX_STRING_LENGTH) {
-	//					if (this.stringPointer > text.length()) {
-	//						this.firstLinePointer += this.stringPointer;
-	//						if (this.tokenPointer < this.tokens.length - 1) {
-	//							this.tokenPointer++;
-	//							this.stringPointer = 0;
-	//						}
-	//						//Short sentences.
-	//						if (this.totalStringPointer >= this.dialogueText.length() - 1) {
-	//							g.drawString(this.dialogueText.substring(this.beginningPointer, this.beginningPointer + this.firstLinePointer), Dialogue.getDialogueTextStartingX(), Dialogue.getDialogueTextStartingY());
-	//							return;
-	//						}
-	//					}
-	//				}
-	//				else {
-	//					if (!this.firstLineFull) {
-	//						this.firstLineFull = true;
-	//						this.secondLinePointer = this.firstLinePointer + this.stringPointer;
-	//					}
-	//				}
-	//				if (this.firstLineFull) {
-	//					//First line is full
-	//					int textCompare = 0;
-	//					if (beginningPointer > (MAX_STRING_LENGTH + 1) * 2)
-	//						textCompare = (((beginningPointer % (MAX_STRING_LENGTH + 1)) + secondLinePointer - 1) % MAX_STRING_LENGTH) + text.length();
-	//					if ((this.totalStringPointer > (MAX_STRING_LENGTH * 2) + 1) ? (textCompare <= MAX_STRING_LENGTH) : (text.length() <= MAX_STRING_LENGTH)) {
-	//						if (this.stringPointer > text.length()) {
-	//							this.secondLinePointer += this.stringPointer;
-	//							if (this.secondLinePointer >= MAX_STRING_LENGTH * 2) {
-	//								//Subtract 2 space characters from the (first + second lines) character total.
-	//								this.secondLineFull = true;
-	//							}
-	//							if (this.tokenPointer < this.tokens.length - 1) {
-	//								this.tokenPointer++;
-	//								this.stringPointer = 0;
-	//							}
-	//						}
-	//					}
-	//					else {
-	//						this.secondLineFull = true;
-	//						if (this.stringPointer > 0)
-	//							this.secondLinePointer += this.stringPointer;
-	//					}
-	//					g.drawString(this.dialogueText.substring(this.beginningPointer, this.beginningPointer + this.firstLinePointer), Dialogue.getDialogueTextStartingX(), Dialogue.getDialogueTextStartingY());
-	//					if (!this.secondLineFull) {
-	//						g.drawString(this.dialogueText.substring(this.beginningPointer + this.firstLinePointer, this.beginningPointer + this.secondLinePointer + this.stringPointer), Dialogue.getDialogueTextStartingX(), Dialogue.getDialogueTextSecondLineStartingY());
-	//					}
-	//					else {
-	//						g.drawString(this.dialogueText.substring(this.beginningPointer + this.firstLinePointer, this.beginningPointer + this.secondLinePointer), Dialogue.getDialogueTextStartingX(), Dialogue.getDialogueTextSecondLineStartingY());
-	//						this.next = true;
-	//					}
-	//				}
-	//				else {
-	//					//First line isn't full
-	//					g.drawString(this.dialogueText.substring(this.beginningPointer, this.beginningPointer + this.firstLinePointer + this.stringPointer), Dialogue.getDialogueTextStartingX(), Dialogue.getDialogueTextStartingY());
-	//				}
-	//			}
-	//		}
-	//	}
-	
-	private int firstLineIterator;
-	private int secondLineIterator;
-	private int dialogKeyID;
 	
 	public void renderText(Graphics g) {
 		g.setColor(Color.black);
@@ -368,35 +194,6 @@ public class Dialogue {
 		}
 	}
 	
-	private String[] toLines(String all) {
-		ArrayList<String> lines = new ArrayList<>();
-		String[] words = all.split("\\s");
-		String line = "";
-		int length = 0;
-		for (String w : words) {
-			if (length + w.length() + 1 > MAX_STRING_LENGTH) {
-				if (w.length() >= MAX_STRING_LENGTH) {
-					line += w;
-					lines.add(line);
-					line = "";
-					continue;
-				}
-				lines.add(line);
-				line = "";
-				length = 0;
-			}
-			if (length > 0) {
-				line += " ";
-				length += 1;
-			}
-			line += w;
-			length += w.length();
-		}
-		if (line.length() > 0)
-			lines.add(line);
-		return lines.toArray(new String[lines.size()]);
-	}
-	
 	public void setDialogKeyID(int value) {
 		this.dialogKeyID = value;
 		this.dialogs.put(value, false);
@@ -440,5 +237,36 @@ public class Dialogue {
 	
 	public boolean getDialogueCheckpoint(int key) {
 		return this.dialogs.get(key);
+	}
+	
+	//----------------------- PRIVATE METHODS ONLY ------------------------------------------
+	
+	private String[] toLines(String all) {
+		ArrayList<String> lines = new ArrayList<>();
+		String[] words = all.split("\\s");
+		String line = "";
+		int length = 0;
+		for (String w : words) {
+			if (length + w.length() + 1 > MAX_STRING_LENGTH) {
+				if (w.length() >= MAX_STRING_LENGTH) {
+					line += w;
+					lines.add(line);
+					line = "";
+					continue;
+				}
+				lines.add(line);
+				line = "";
+				length = 0;
+			}
+			if (length > 0) {
+				line += " ";
+				length += 1;
+			}
+			line += w;
+			length += w.length();
+		}
+		if (line.length() > 0)
+			lines.add(line);
+		return lines.toArray(new String[lines.size()]);
 	}
 }
