c1ecb228fcdc3df95cd4a6549e09fe2f9fd365f3
tommai78101 <tom.mai78101@gmail.com>
2014-06-01 17:19:37 +0800

    Fixed inconsistent packet size.

--- src/saving/GameSave.java
+++ src/saving/GameSave.java
@@ -8,6 +8,7 @@ import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+
 import level.Area;
 import main.Game;
 import abstracts.Item;
@@ -18,19 +19,18 @@ public class GameSave {
 	/*
 	 * Refer to the documentation on the file format used for saving game data.
 	 * 
-	 * All saving/loading schemes use the documentation as guidelines. It will be
-	 * changed in the future, as the development continues.
-	 * */
+	 * All saving/loading schemes use the documentation as guidelines. It will be changed in the future, as the development continues.
+	 */
 	public static class HeaderInfo {
 		public final byte[] header_id = "IDNO".getBytes();
 		public final byte[] header_code = "HEAD".getBytes();
 		public final byte[] header_format = ".SAV".getBytes();
 		public final int size;
-		
+
 		public HeaderInfo() {
 			size = header_id.length + header_code.length + header_format.length;
 		}
-		
+
 		public void write(RandomAccessFile raf) throws IOException {
 			raf.write(size);
 			raf.write(header_id);
@@ -38,7 +38,7 @@ public class GameSave {
 			raf.write(header_format);
 		}
 	}
-	
+
 	public static class PlayerInfo {
 		public final byte[] player_name = new byte[16];
 		public final byte[] player_gender = new byte[1];
@@ -52,7 +52,7 @@ public class GameSave {
 		public final byte[] player_x = new byte[4];
 		public final byte[] player_y = new byte[4];
 		public final byte[] player_facing = new byte[4];
-		
+
 		public static final byte[] PLAY = "PLAY".getBytes(); //Player Info
 		public static final byte[] NAME = "NAME".getBytes(); //Player name.
 		public static final byte[] GNDR = "GNDR".getBytes(); //Player gender.
@@ -61,25 +61,25 @@ public class GameSave {
 		public static final byte[] AREA = "AREA".getBytes(); //Current Area.
 		public static final byte[] AXIS = "AXIS".getBytes(); //Current Position.
 		public static final byte[] TURN = "TURN".getBytes(); //Current Direction.
-		
+
 		private int byteSize = 0;
-		
+
 		public void write(RandomAccessFile raf) throws IOException {
 			//Sizes are added at the beginning of every little bit of data.
 			//Player Info Header
 			raf.writeShort(this.byteSize + PLAY.length);
 			raf.write(PLAY);
-			
+
 			//Player name.
 			raf.writeByte(player_name.length + NAME.length);
 			raf.write(NAME);
 			raf.write(player_name);
-			
+
 			//Player gender.
 			raf.writeByte(player_gender.length + GNDR.length);
 			raf.write(GNDR);
 			raf.write(player_gender);
-			
+
 			//Unlocked start menu options.
 			int size = 0;
 			for (int i = 0; i < this.startMenu.size(); i++)
@@ -91,7 +91,7 @@ public class GameSave {
 				raf.writeByte(buf.length);
 				raf.write(buf);
 			}
-			
+
 			//Inventory
 			size = 0;
 			for (int i = 0; i < 4; i++) {
@@ -101,7 +101,7 @@ public class GameSave {
 					for (int j = 0; j < this.getAllItemsList().get(i).size(); j++)
 						size += this.getAllItemsList().get(i).get(j).length + 1;
 				}
-				
+
 			}
 			raf.writeByte(size + ITEM.length);
 			raf.write(ITEM);
@@ -116,37 +116,37 @@ public class GameSave {
 					}
 				}
 			}
-			
+
 			//Current Area Info
 			raf.writeByte(AREA.length + player_current_area_id.length + player_current_area_sector_id.length);
 			raf.write(AREA);
 			raf.write(player_current_area_id);
 			raf.write(player_current_area_sector_id);
-			
+
 			//Current Position.
 			raf.writeByte(AXIS.length + player_x.length + player_y.length);
 			raf.write(AXIS);
 			raf.write(player_x);
 			raf.write(player_y);
-			
+
 			//Current Player Direction Facing.
 			raf.writeByte(TURN.length + player_facing.length);
 			raf.write(TURN);
 			raf.write(player_facing);
 		}
-		
+
 		public void increment(byte[] list) {
 			this.byteSize += list.length;
 		}
-		
+
 		public void reset() {
 			this.byteSize = 0;
 		}
-		
+
 		public int getByteSize() {
 			return this.byteSize;
 		}
-		
+
 		public List<ArrayList<byte[]>> getAllItemsList() {
 			List<ArrayList<byte[]>> result = new ArrayList<ArrayList<byte[]>>();
 			result.add(this.potions);
@@ -156,65 +156,64 @@ public class GameSave {
 			return result;
 		}
 	}
-	
-	public static final byte[] SIGNATURE = new byte[]{
-			(byte) 137, 0x53, 0x41, 0x56, 0x20, 0x20, 0x20, 0x20
-	};
-	
+
+	public static final byte[] SIGNATURE = new byte[] { (byte) 137, 0x53, 0x41, 0x56, 0x20, 0x20, 0x20, 0x20 };
+
 	public HeaderInfo headerInfo;
 	public PlayerInfo playerInfo;
-	
+
 	private GameSave() {
 		headerInfo = new HeaderInfo();
 		playerInfo = new PlayerInfo();
 	}
-	
+
 	private void write(RandomAccessFile raf) throws IOException {
 		raf.seek(0);
 		headerInfo.write(raf);
 		playerInfo.write(raf);
 	}
-	
+
 	@SuppressWarnings("unused")
 	private void handleLoadedBuffers(Game game, ArrayList<byte[]> buffers) {
 		//TODO: For each byte[] array in the array list, take apart the data so that the game can read the data and load from there.
 	}
-	
+
 	private void generateSaveData(Game game) {
 		//TODO: Write all game data to byte equivalent values and insert it into a byte array.
-		
+
 		Player gamePlayer = game.getPlayer();
 		if (this.playerInfo != null)
 			this.playerInfo.reset();
-		
+
 		//Name of the player.
 		System.arraycopy(gamePlayer.getByteName(), 0, this.playerInfo.player_name, 0, 16); //16 is the name length limit.
 		byte[] byteArray = concatenate(PlayerInfo.NAME, gamePlayer.getByteName());
-		this.playerInfo.increment(concatenate(new byte[]{0x0}, byteArray));
-		
+		this.playerInfo.increment(concatenate(new byte[] { 0x0 }, byteArray));
+
 		//Gender of the player.
 		System.arraycopy(gamePlayer.getByteGender(), 0, this.playerInfo.player_gender, 0, 1); //1 is the gender boolean size.
 		byteArray = concatenate(PlayerInfo.GNDR, gamePlayer.getByteGender());
-		this.playerInfo.increment(concatenate(new byte[]{0x0}, byteArray));
-		
+		this.playerInfo.increment(concatenate(new byte[] { 0x0 }, byteArray));
+
 		//All active submenus the player has unlocked.
-		byteArray = new byte[]{};
+		byteArray = new byte[] {};
 		List<Map.Entry<Integer, SubMenu>> startMenuList = game.getStartMenu().getSubMenusList();
 		for (Map.Entry<Integer, SubMenu> entry : startMenuList) {
 			SubMenu sub = entry.getValue();
-			byteArray = concatenate(byteArray, concatenate(new byte[]{0x0}, sub.getSubMenuData()));
+			byteArray = concatenate(byteArray, concatenate(new byte[] { 0x0 }, sub.getSubMenuData()));
 			this.playerInfo.startMenu.add(sub.getSubMenuData());
 		}
-		this.playerInfo.increment(concatenate(new byte[]{0x0}, concatenate(PlayerInfo.MENU, byteArray)));
-		
+		this.playerInfo.increment(concatenate(new byte[] { 0x0 }, concatenate(PlayerInfo.MENU, byteArray)));
+
 		//Inventory
 		//FIXME: Incorrect size of packets from inventory. Please check for inconsistencies.
 		byte listType = 0x1;
-		this.playerInfo.increment(concatenate(new byte[]{0x0}, PlayerInfo.ITEM));
+		this.playerInfo.increment(concatenate(new byte[] { 0x0 }, PlayerInfo.ITEM));
 		for (List<Map.Entry<Item, Integer>> itemList : game.getStartMenu().getInventory().getAllItemsList()) {
 			if (itemList.size() - 1 > 0) {
-				byteArray = new byte[]{listType}; //Type of list (Potions/KeyItems/Pokeball/TMHM)
+				byteArray = new byte[] { listType }; //Type of list (Potions/KeyItems/Pokeball/TMHM)
 				byteArray = concatenate(byteArray, ByteBuffer.allocate(2).putChar((char) ((itemList.size() - 1) & 0xFFFF)).array()); //How many items in a list?
+				byteArray = concatenate(byteArray, ByteBuffer.allocate(1).put((byte) itemList.size()).array());
 				byte[] itemInfo = null;
 				for (int i = 0; i < itemList.size() - 1; i++) {
 					Map.Entry<Item, Integer> entry = itemList.get(i);
@@ -233,7 +232,7 @@ public class GameSave {
 				listType++;
 			}
 		}
-		
+
 		//Current Area & Player State
 		Area currentArea = game.getWorld().getCurrentArea();
 		byte[] bufArea = ByteBuffer.allocate(4).putInt(currentArea.getAreaID()).array();
@@ -248,20 +247,20 @@ public class GameSave {
 			this.playerInfo.player_y[i] = bufY[i];
 			this.playerInfo.player_facing[i] = bufFacing[i];
 		}
-		byteArray = new byte[]{};
+		byteArray = new byte[] {};
 		byteArray = concatenate(byteArray, bufArea);
 		byteArray = concatenate(byteArray, bufSector);
-		this.playerInfo.increment(concatenate(concatenate(new byte[]{0x0}, PlayerInfo.AREA), byteArray));
-		
-		byteArray = new byte[]{};
+		this.playerInfo.increment(concatenate(concatenate(new byte[] { 0x0 }, PlayerInfo.AREA), byteArray));
+
+		byteArray = new byte[] {};
 		byteArray = concatenate(byteArray, bufX);
 		byteArray = concatenate(byteArray, bufY);
-		this.playerInfo.increment(concatenate(concatenate(new byte[]{0x0}, PlayerInfo.AXIS), byteArray));
-		
+		this.playerInfo.increment(concatenate(concatenate(new byte[] { 0x0 }, PlayerInfo.AXIS), byteArray));
+
 		//Player State
-		this.playerInfo.increment(concatenate(concatenate(new byte[]{0x0}, PlayerInfo.TURN), bufFacing));
+		this.playerInfo.increment(concatenate(concatenate(new byte[] { 0x0 }, PlayerInfo.TURN), bufFacing));
 	}
-	
+
 	public static void save(Game game, String filename) {
 		GameSave data = new GameSave();
 		data.generateSaveData(game);
@@ -287,22 +286,21 @@ public class GameSave {
 				e.printStackTrace();
 			}
 		}
-		
+
 	}
-	
+
 	public static void load(Game game, String filename) {
 		File load = new File(filename);
 		if (load.isFile()) {
-			
+
 		}
 	}
-	
+
 	/**
 	 * Checks to see if the game saved data exists in relative to where the game is located.
 	 * 
 	 * <p>
-	 * Note that {@link java.io.File#isFile() File.isFile()} is used to check for saved data that is generated by the game. This is because the saved
-	 * data file generated by the game is a <i>normal</i> file.
+	 * Note that {@link java.io.File#isFile() File.isFile()} is used to check for saved data that is generated by the game. This is because the saved data file generated by the game is a <i>normal</i> file.
 	 * 
 	 * @param filename
 	 *            The file name of the saved data.
@@ -312,7 +310,7 @@ public class GameSave {
 		File save = new File(filename);
 		return save.isFile();
 	}
-	
+
 	public static byte[] concatenate(byte[] first, byte[] second) {
 		byte[] result = new byte[first.length + second.length];
 		System.arraycopy(first, 0, result, 0, first.length);
