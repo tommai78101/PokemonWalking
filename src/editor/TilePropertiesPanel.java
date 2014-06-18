49875f2af90552cd480d8a07600b561c05123026
tom_mai78101 <tom.mai78101@gmail.com>
2014-06-18 14:00:02 +0800

    Editor: Separated data inputs from displaying tile data values. This helps fix issues with the drop-down menus.

--- src/editor/TilePropertiesPanel.java
+++ src/editor/TilePropertiesPanel.java
@@ -20,8 +20,6 @@ import javax.swing.SwingConstants;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 
-import editor.EditorConstants.Tools;
-
 public class TilePropertiesPanel extends JPanel implements DocumentListener {
 	private static final long serialVersionUID = 1L;
 	
@@ -29,175 +27,113 @@ public class TilePropertiesPanel extends JPanel implements DocumentListener {
 	public JTextField redField;
 	public JTextField greenField;
 	public JTextField blueField;
-	public Data selectedData;
-	public JLabel tileID, extendedTileID, tileSpecificID;
+	
+	public CustomJTextField alphaInputField;
+	public CustomJTextField redInputField;
+	public CustomJTextField greenInputField;
+	public CustomJTextField blueInputField;
+	
+	public int dataValue;
+	
+	public JLabel tileID, extendedTileID, tileSpecificID, fullDataInput;
+	
 	private static final Dimension SIZE = new Dimension(70, 10);
 	private static final Dimension INPUT_SIZE = new Dimension(15, 15);
+	
+	public class CustomJLabel extends JLabel {
+		private static final long serialVersionUID = 1L;
+		
+		public CustomJLabel(String string) {
+			super(string);
+		}
+		
+		@Override
+		public Dimension getSize() {
+			return SIZE;
+		}
+		
+		@Override
+		public Dimension getPreferredSize() {
+			return SIZE;
+		}
+		
+		@Override
+		public Dimension getMaximumSize() {
+			return SIZE;
+		}
+		
+		@Override
+		public Dimension getMinimumSize() {
+			return SIZE;
+		}
+	}
+	
+	public class CustomJTextField extends JTextField {
+		private static final long serialVersionUID = 1L;
+		
+		@Override
+		public Dimension getSize() {
+			return INPUT_SIZE;
+		}
+		
+		@Override
+		public Dimension getPreferredSize() {
+			return INPUT_SIZE;
+		}
+		
+		@Override
+		public Dimension getMaximumSize() {
+			return INPUT_SIZE;
+		}
+		
+		@Override
+		public Dimension getMinimumSize() {
+			return INPUT_SIZE;
+		}
+	}
 	
 	@SuppressWarnings({ "serial" })
 	public TilePropertiesPanel() {
 		super();
 		
-		this.selectedData = new Data();
+		tileID = new CustomJLabel("Tile ID:");
+		extendedTileID = new CustomJLabel("Ext. ID:");
+		tileSpecificID = new CustomJLabel("Other ID:");
+		fullDataInput = new CustomJLabel("Edit Data:");
 		
-		tileID = new JLabel("Tile ID:") {
-			@Override
-			public Dimension getSize() {
-				return SIZE;
-			}
-			
-			@Override
-			public Dimension getPreferredSize() {
-				return SIZE;
-			}
-			
-			@Override
-			public Dimension getMaximumSize() {
-				return SIZE;
-			}
-			
-			@Override
-			public Dimension getMinimumSize() {
-				return SIZE;
-			}
-		};
-		extendedTileID = new JLabel("Ext. ID:") {
-			@Override
-			public Dimension getSize() {
-				return SIZE;
-			}
-			
-			@Override
-			public Dimension getPreferredSize() {
-				return SIZE;
-			}
-			
-			@Override
-			public Dimension getMaximumSize() {
-				return SIZE;
-			}
-			
-			@Override
-			public Dimension getMinimumSize() {
-				return SIZE;
-			}
-		};
-		tileSpecificID = new JLabel("Other ID:") {
-			@Override
-			public Dimension getSize() {
-				return SIZE;
-			}
-			
-			@Override
-			public Dimension getPreferredSize() {
-				return SIZE;
-			}
-			
-			@Override
-			public Dimension getMaximumSize() {
-				return SIZE;
-			}
-			
-			@Override
-			public Dimension getMinimumSize() {
-				return SIZE;
-			}
-		};
-		alphaField = new JTextField() {
-			@Override
-			public Dimension getSize() {
-				return INPUT_SIZE;
-			}
-			
-			@Override
-			public Dimension getPreferredSize() {
-				return INPUT_SIZE;
-			}
-			
-			@Override
-			public Dimension getMaximumSize() {
-				return INPUT_SIZE;
-			}
-			
-			@Override
-			public Dimension getMinimumSize() {
-				return INPUT_SIZE;
-			}
-		};
-		redField = new JTextField() {
-			@Override
-			public Dimension getSize() {
-				return INPUT_SIZE;
-			}
-			
-			@Override
-			public Dimension getPreferredSize() {
-				return INPUT_SIZE;
-			}
-			
-			@Override
-			public Dimension getMaximumSize() {
-				return INPUT_SIZE;
-			}
-			
-			@Override
-			public Dimension getMinimumSize() {
-				return INPUT_SIZE;
-			}
-		}; // RR
-		greenField = new JTextField() {
-			@Override
-			public Dimension getSize() {
-				return INPUT_SIZE;
-			}
-			
-			@Override
-			public Dimension getPreferredSize() {
-				return INPUT_SIZE;
-			}
-			
-			@Override
-			public Dimension getMaximumSize() {
-				return INPUT_SIZE;
-			}
-			
-			@Override
-			public Dimension getMinimumSize() {
-				return INPUT_SIZE;
-			}
-		}; // GG
-		blueField = new JTextField() {
-			@Override
-			public Dimension getSize() {
-				return INPUT_SIZE;
-			}
-			
-			@Override
-			public Dimension getPreferredSize() {
-				return INPUT_SIZE;
-			}
-			
-			@Override
-			public Dimension getMaximumSize() {
-				return INPUT_SIZE;
-			}
-			
-			@Override
-			public Dimension getMinimumSize() {
-				return INPUT_SIZE;
-			}
-		}; // BB
+		alphaField = new CustomJTextField(); // AA
+		redField = new CustomJTextField(); // RR
+		greenField = new CustomJTextField(); // GG
+		blueField = new CustomJTextField(); // BB
+		
+		alphaInputField = new CustomJTextField();
+		redInputField = new CustomJTextField();
+		greenInputField = new CustomJTextField();
+		blueInputField = new CustomJTextField();
+		
 		tileID.setHorizontalAlignment(SwingConstants.CENTER);
 		extendedTileID.setHorizontalAlignment(SwingConstants.CENTER);
 		tileSpecificID.setHorizontalAlignment(SwingConstants.CENTER);
+		
 		alphaField.setHorizontalAlignment(SwingConstants.CENTER);
 		redField.setHorizontalAlignment(SwingConstants.CENTER);
 		greenField.setHorizontalAlignment(SwingConstants.CENTER);
 		blueField.setHorizontalAlignment(SwingConstants.CENTER);
-		alphaField.getDocument().addDocumentListener(this);
-		redField.getDocument().addDocumentListener(this);
-		greenField.getDocument().addDocumentListener(this);
-		blueField.getDocument().addDocumentListener(this);
+		
+		alphaField.setEditable(false);
+		redField.setEditable(false);
+		greenField.setEditable(false);
+		blueField.setEditable(false);
+		
+		alphaInputField.setHorizontalAlignment(SwingConstants.CENTER);
+		redInputField.setHorizontalAlignment(SwingConstants.CENTER);
+		greenInputField.setHorizontalAlignment(SwingConstants.CENTER);
+		blueInputField.setHorizontalAlignment(SwingConstants.CENTER);
+		
+		 alphaInputField.getDocument().addDocumentListener(this);
+		 redInputField.getDocument().addDocumentListener(this);
+		 greenInputField.getDocument().addDocumentListener(this);
+		 blueInputField.getDocument().addDocumentListener(this);
 		
 		this.setLayout(new GridLayout(0, 1));
 		this.add(tileID);
@@ -207,11 +143,18 @@ public class TilePropertiesPanel extends JPanel implements DocumentListener {
 		this.add(tileSpecificID);
 		this.add(greenField);
 		this.add(blueField);
+		this.add(fullDataInput);
+		this.add(alphaInputField);
+		this.add(redInputField);
+		this.add(greenInputField);
+		this.add(blueInputField);
+		
+		this.validate();
 	}
 	
 	public char getAlpha() {
 		try {
-			return (char) (Integer.valueOf(alphaField.getText()) & 0xFF);
+			return (char) (Integer.valueOf(alphaInputField.getText()) & 0xFF);
 		}
 		catch (NumberFormatException e) {
 			return 0;
@@ -224,7 +167,7 @@ public class TilePropertiesPanel extends JPanel implements DocumentListener {
 	
 	public char getRed() {
 		try {
-			return (char) (Integer.valueOf(redField.getText()) & 0xFF);
+			return (char) (Integer.valueOf(redInputField.getText()) & 0xFF);
 		}
 		catch (NumberFormatException e) {
 			return 0;
@@ -237,7 +180,7 @@ public class TilePropertiesPanel extends JPanel implements DocumentListener {
 	
 	public char getGreen() {
 		try {
-			return (char) (Integer.valueOf(greenField.getText()) & 0xFF);
+			return (char) (Integer.valueOf(greenInputField.getText()) & 0xFF);
 		}
 		catch (NumberFormatException e) {
 			return 0;
@@ -250,131 +193,86 @@ public class TilePropertiesPanel extends JPanel implements DocumentListener {
 	
 	public char getBlue() {
 		try {
-			return (char) (Integer.valueOf(blueField.getText()) & 0xFF);
+			return (char) (Integer.valueOf(blueInputField.getText()) & 0xFF);
 		}
 		catch (NumberFormatException e) {
 			return 0;
 		}
 	}
 	
-	public String getTileIDBString() {
-		return blueField.getText();
-	}
-	
-	public Data getSelectedData() {
-		return this.selectedData;
+	public void clearInputFields() {
+		this.alphaInputField.setText("");
+		this.redInputField.setText("");
+		this.greenInputField.setText("");
+		this.blueInputField.setText("");
 	}
 	
 	@Override
-	public void changedUpdate(DocumentEvent arg0) {
-		EditorConstants.chooser = Tools.TileProperties;
+	public void changedUpdate(DocumentEvent event) {
 		try {
-			int alpha = Integer.parseInt(this.alphaField.getText());
-			int red = Integer.parseInt(this.redField.getText());
-			int green = Integer.parseInt(this.greenField.getText());
-			int blue = Integer.parseInt(this.blueField.getText());
-			this.selectedData = EditorConstants.getData(alpha, red, green, blue);
-			if (selectedData.areaTypeIncluded) {
-				switch (selectedData.areaTypeIDType) {
-					case ALPHA:
-					default:
-						selectedData.alpha = alpha;
-						break;
-					case RED:
-						selectedData.red = red;
-						break;
-					case GREEN:
-						selectedData.green = green;
-						break;
-					case BLUE:
-						selectedData.blue = blue;
-						break;
-				}
-			}
+			byte a = (byte) (Integer.valueOf(this.alphaInputField.getText()) & 0xFF);
+			byte r = (byte) (Integer.valueOf(this.redInputField.getText()) & 0xFF);
+			byte g = (byte) (Integer.valueOf(this.greenInputField.getText()) & 0xFF);
+			byte b = (byte) (Integer.valueOf(this.blueInputField.getText()) & 0xFF);
+			this.dataValue = (a << 24) | (r << 16) | (g << 8) | b;
 		}
 		catch (Exception e) {
-			EditorConstants.chooser = Tools.ControlPanel;
+			dataValue = 0;
 		}
-		
 	}
 	
 	@Override
-	public void insertUpdate(DocumentEvent arg0) {
-		EditorConstants.chooser = Tools.TileProperties;
+	public void insertUpdate(DocumentEvent event) {
 		try {
-			int alpha = Integer.parseInt(this.alphaField.getText());
-			int red = Integer.parseInt(this.redField.getText());
-			int green = Integer.parseInt(this.greenField.getText());
-			int blue = Integer.parseInt(this.blueField.getText());
-			this.selectedData = EditorConstants.getData(alpha, red, green, blue);
-			if (selectedData.areaTypeIncluded) {
-				switch (selectedData.areaTypeIDType) {
-					case ALPHA:
-					default:
-						selectedData.alpha = alpha;
-						break;
-					case RED:
-						selectedData.red = red;
-						break;
-					case GREEN:
-						selectedData.green = green;
-						break;
-					case BLUE:
-						selectedData.blue = blue;
-						break;
-				}
-			}
+			byte a = (byte) (Integer.valueOf(this.alphaInputField.getText()) & 0xFF);
+			byte r = (byte) (Integer.valueOf(this.redInputField.getText()) & 0xFF);
+			byte g = (byte) (Integer.valueOf(this.greenInputField.getText()) & 0xFF);
+			byte b = (byte) (Integer.valueOf(this.blueInputField.getText()) & 0xFF);
+			this.dataValue = (a << 24) | (r << 16) | (g << 8) | b;
 		}
 		catch (Exception e) {
-			EditorConstants.chooser = Tools.ControlPanel;
+			dataValue = 0;
 		}
 	}
 	
 	@Override
-	public void removeUpdate(DocumentEvent arg0) {
-		EditorConstants.chooser = Tools.TileProperties;
+	public void removeUpdate(DocumentEvent event) {
 		try {
-			int alpha = Integer.parseInt(this.alphaField.getText());
-			int red = Integer.parseInt(this.redField.getText());
-			int green = Integer.parseInt(this.greenField.getText());
-			int blue = Integer.parseInt(this.blueField.getText());
-			this.selectedData = EditorConstants.getData(alpha, red, green, blue);
-			if (selectedData.areaTypeIncluded) {
-				switch (selectedData.areaTypeIDType) {
-					case ALPHA:
-					default:
-						selectedData.alpha = alpha;
-						break;
-					case RED:
-						selectedData.red = red;
-						break;
-					case GREEN:
-						selectedData.green = green;
-						break;
-					case BLUE:
-						selectedData.blue = blue;
-						break;
-				}
-			}
+			byte a = (byte) (Integer.valueOf(this.alphaInputField.getText()) & 0xFF);
+			byte r = (byte) (Integer.valueOf(this.redInputField.getText()) & 0xFF);
+			byte g = (byte) (Integer.valueOf(this.greenInputField.getText()) & 0xFF);
+			byte b = (byte) (Integer.valueOf(this.blueInputField.getText()) & 0xFF);
+			this.dataValue = (a << 24) | (r << 16) | (g << 8) | b;
 		}
 		catch (Exception e) {
-			EditorConstants.chooser = Tools.ControlPanel;
+			dataValue = 0;
 		}
 	}
 	
 	@Override
-	public void validate(){
+	public void validate() {
 		super.validate();
-		switch (EditorConstants.metadata){
+		switch (EditorConstants.metadata) {
 			case Pixel_Data:
 				this.tileID.setText("Tile ID:");
 				this.extendedTileID.setText("Extended ID:");
 				this.tileSpecificID.setText("Other IDs:");
+				this.fullDataInput.setVisible(true);
+				this.alphaInputField.setVisible(true);
+				this.redInputField.setVisible(true);
+				this.greenInputField.setVisible(true);
+				this.blueInputField.setVisible(true);
 				break;
 			case Triggers:
 				this.tileID.setText("X Position:");
 				this.extendedTileID.setText("Y Position:");
 				this.tileSpecificID.setText("Trigger ID:");
+				this.fullDataInput.setVisible(false);
+				this.alphaInputField.setVisible(false);
+				this.redInputField.setVisible(false);
+				this.greenInputField.setVisible(false);
+				this.blueInputField.setVisible(false);
+				
 				break;
 		}
 	}
